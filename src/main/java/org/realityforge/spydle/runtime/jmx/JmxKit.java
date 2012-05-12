2db94bcbd417e11b1160bf3c528ebba6559fd050
Peter Donald <peter@realityforge.org>
2012-05-13 08:58:40 +1000

    Extract out ConfigUtil utility class

--- src/main/java/org/realityforge/spydle/runtime/jmx/JmxKit.java
+++ src/main/java/org/realityforge/spydle/runtime/jmx/JmxKit.java
@@ -2,15 +2,13 @@ package org.realityforge.spydle.runtime.jmx;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.realityforge.spydle.runtime.Namespace;
+import org.realityforge.spydle.runtime.util.ConfigUtil;
 
 /**
  * Utility class to interact with the JmxSource.
@@ -24,17 +22,17 @@ public final class JmxKit
   public static JmxTaskDescriptor parse( final JSONObject config )
     throws Exception
   {
-    final String host = getValue( config, "host", String.class );
-    final int port = getValue( config, "port", Number.class ).intValue();
+    final String host = ConfigUtil.getValue( config, "host", String.class );
+    final int port = ConfigUtil.getValue( config, "port", Number.class ).intValue();
 
-    final String username = getValue( config, "username", String.class, false );
-    final String password = getValue( config, "password", String.class, false );
+    final String username = ConfigUtil.getValue( config, "username", String.class, false );
+    final String password = ConfigUtil.getValue( config, "password", String.class, false );
 
     final JmxServiceDescriptor serviceDescriptor = new JmxServiceDescriptor( host, port, username, password );
 
     final List<JmxQuery> queries = new ArrayList<>();
 
-    final JSONArray queryArray = getValue( config, "probes", JSONArray.class );
+    final JSONArray queryArray = ConfigUtil.getValue( config, "probes", JSONArray.class );
     for( final Object queryConfig : queryArray )
     {
       queries.add( parseQuery( (JSONObject) queryConfig ) );
@@ -46,8 +44,8 @@ public final class JmxKit
   private static JmxQuery parseQuery( final JSONObject config )
     throws MalformedObjectNameException
   {
-    final String objectName = getValue( config, "object_name", String.class );
-    final JSONArray queryArray = getValue( config, "attribute_names", JSONArray.class, false );
+    final String objectName = ConfigUtil.getValue( config, "object_name", String.class );
+    final JSONArray queryArray = ConfigUtil.getValue( config, "attribute_names", JSONArray.class, false );
     final HashSet<String> attributeNames;
     if( null != queryArray )
     {
@@ -61,91 +59,8 @@ public final class JmxKit
     {
       attributeNames = null;
     }
-    final Namespace namespace = parseNamespace( config );
+    final Namespace namespace = ConfigUtil.parseNamespace( config );
     final ArrayList<String> nameComponents = new ArrayList<>();
     return new JmxQuery( new ObjectName( objectName ), attributeNames, namespace, nameComponents );
-  }
-
-  /**
-   * Parse the "namespace" field from the specified configuration.
-   * If no namespace defined, return an empty namespace.
-   *
-   * @param config the configuration.
-   * @return the namespace.
-   */
-  private static Namespace parseNamespace( final JSONObject config )
-  {
-    final LinkedHashMap<String, String> map = new LinkedHashMap<>();
-    final JSONObject namespace = getValue( config, "namespace", JSONObject.class, false );
-    if( null != namespace )
-    {
-      for( final Object key : namespace.keySet() )
-      {
-        map.put( key.toString(), String.valueOf( namespace.get( key ) ) );
-      }
-    }
-    return new Namespace( map );
-  }
-
-
-  /**
-   * Retrieve a value from json object with the expected type.
-   *
-   * @param config the json object.
-   * @param key the key to access.
-   * @param type the expected type of the value.
-   * @param <T> the type of the value
-   * @return the value.
-   * @throws IllegalArgumentException if value is the incorrect type or the value is missing.
-   */
-  @Nonnull
-  private static <T> T getValue( @Nonnull final JSONObject config,
-                                 @Nonnull final String key,
-                                 @Nonnull final Class<T> type )
-  {
-    final T value = getValue( config, key, type, true );
-    assert null != value;
-    return value;
-  }
-
-  /**
-   * Retrieve a value from json object with the expected type.
-   * If ensure parameter is true then the method will raise an exception if the value is missing.
-   *
-   * @param config the json object.
-   * @param key the key to access.
-   * @param type the expected type of the value.
-   * @param ensure true means an exception is raised if the value is missing.
-   * @param <T> the type of the value
-   * @return the value.
-   * @throws IllegalArgumentException if value is the incorrect type or the value is missing and ensure is true.
-   */
-  @Nullable
-  private static <T> T getValue( @Nonnull final JSONObject config,
-                                 @Nonnull final String key,
-                                 @Nonnull final Class<T> type,
-                                 final boolean ensure )
-  {
-    final Object value = config.get( key );
-    if( null == value )
-    {
-      if( ensure )
-      {
-        throw new IllegalArgumentException( "Missing '" + key + "' parameter from configuration: " + config );
-      }
-      else
-      {
-        return null;
-      }
-    }
-    else if( !type.isInstance( value ) )
-    {
-      throw new IllegalArgumentException( "'" + key + "' parameter from configuration is not of the expected type: " + config );
-    }
-    else
-    {
-      //noinspection unchecked
-      return (T) value;
-    }
   }
 }
---
+++ src/main/java/org/realityforge/spydle/runtime/util/ConfigUtil.java
@@ -0,0 +1,99 @@
+package org.realityforge.spydle.runtime.util;
+
+import java.util.LinkedHashMap;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import org.json.simple.JSONObject;
+import org.realityforge.spydle.runtime.Namespace;
+
+/**
+ * Utility class for processing json configuration files.
+ */
+public final class ConfigUtil
+{
+  private ConfigUtil()
+  {
+  }
+
+  /**
+   * Parse the "namespace" field from the specified configuration.
+   * If no namespace defined, return an empty namespace.
+   *
+   * @param config the configuration.
+   * @return the namespace.
+   */
+  public static Namespace parseNamespace( final JSONObject config )
+  {
+    final LinkedHashMap<String, String> map = new LinkedHashMap<>();
+    final JSONObject namespace = getValue( config, "namespace", JSONObject.class, false );
+    if( null != namespace )
+    {
+      for( final Object key : namespace.keySet() )
+      {
+        map.put( key.toString(), String.valueOf( namespace.get( key ) ) );
+      }
+    }
+    return new Namespace( map );
+  }
+
+  /**
+   * Retrieve a value from json object with the expected type.
+   *
+   * @param config the json object.
+   * @param key    the key to access.
+   * @param type   the expected type of the value.
+   * @param <T>    the type of the value
+   * @return the value.
+   * @throws IllegalArgumentException if value is the incorrect type or the value is missing.
+   */
+  @Nonnull
+  public static <T> T getValue( @Nonnull final JSONObject config,
+                                @Nonnull final String key,
+                                @Nonnull final Class<T> type )
+  {
+    final T value = getValue( config, key, type, true );
+    assert null != value;
+    return value;
+  }
+
+  /**
+   * Retrieve a value from json object with the expected type.
+   * If ensure parameter is true then the method will raise an exception if the value is missing.
+   *
+   * @param config the json object.
+   * @param key    the key to access.
+   * @param type   the expected type of the value.
+   * @param ensure true means an exception is raised if the value is missing.
+   * @param <T>    the type of the value
+   * @return the value.
+   * @throws IllegalArgumentException if value is the incorrect type or the value is missing and ensure is true.
+   */
+  @Nullable
+  public static <T> T getValue( @Nonnull final JSONObject config,
+                                @Nonnull final String key,
+                                @Nonnull final Class<T> type,
+                                final boolean ensure )
+  {
+    final Object value = config.get( key );
+    if( null == value )
+    {
+      if( ensure )
+      {
+        throw new IllegalArgumentException( "Missing '" + key + "' parameter from configuration: " + config );
+      }
+      else
+      {
+        return null;
+      }
+    }
+    else if( !type.isInstance( value ) )
+    {
+      throw new IllegalArgumentException( "'" + key + "' parameter from configuration is not of the expected type: " + config );
+    }
+    else
+    {
+      //noinspection unchecked
+      return (T) value;
+    }
+  }
+}
