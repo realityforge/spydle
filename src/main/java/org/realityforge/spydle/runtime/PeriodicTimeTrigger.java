5c984ac36876ba826761a4d750c738e5c5fa6982
Peter Donald <peter@realityforge.org>
2012-05-17 16:48:26 +1000

    Add a periodic trigger for system actions

---
+++ src/main/java/org/realityforge/spydle/runtime/PeriodicTimeTrigger.java
@@ -0,0 +1,33 @@
+package org.realityforge.spydle.runtime;
+
+import javax.annotation.Nonnegative;
+
+public final class PeriodicTimeTrigger
+  implements TimeTrigger
+{
+  private final int _period;
+  private long _triggerTime;
+
+  public PeriodicTimeTrigger( @Nonnegative final int period )
+  {
+    _period = period;
+    _triggerTime = System.currentTimeMillis();
+  }
+
+  public long getTimeAfter( final long moment )
+  {
+    if( moment <= _triggerTime )
+    {
+      return _triggerTime;
+    }
+    else
+    {
+      final long over = moment - _triggerTime;
+      final long remainder = over % _period;
+      return moment + ( _period - remainder );
+    }
+  }
+}
+
+
+
