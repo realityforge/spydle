7aa5a197c44b5a6a5db17cff0c1acba6b288db79
Peter Donald <peter@realityforge.org>
2012-07-19 22:05:34 +1000

    Add some comparators and toString to support class

--- src/main/java/org/realityforge/spydle/syslog/StructuredDataParameter.java
+++ src/main/java/org/realityforge/spydle/syslog/StructuredDataParameter.java
@@ -26,4 +26,34 @@ public final class StructuredDataParameter
   {
     return _value;
   }
+
+  @Override
+  public boolean equals( final Object o )
+  {
+    if( this == o )
+    {
+      return true;
+    }
+    else if( o == null || getClass() != o.getClass() )
+    {
+      return false;
+    }
+
+    final StructuredDataParameter that = (StructuredDataParameter) o;
+    return _name.equals( that._name ) && _value.equals( that._value );
+  }
+
+  @Override
+  public int hashCode()
+  {
+    int result = _name.hashCode();
+    result = 31 * result + _value.hashCode();
+    return result;
+  }
+
+  @Override
+  public String toString()
+  {
+    return getName() + "=" + getValue();
+  }
 }
