6bd9d8169f77d591cb2884dfd756773c5ef2c4e6
Peter Donald <peter@realityforge.org>
2012-07-19 17:45:10 +1000

    Collapse parser into SyslogMessage

--- src/test/java/org/realityforge/spydle/syslog/SyslogMessageTest.java
+++ src/test/java/org/realityforge/spydle/syslog/SyslogMessageTest.java
@@ -7,11 +7,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
-public class SyslogParserTest
-
-/*
-<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"][examplePriority@32473 class="high"]
- */
+public class SyslogMessageTest
 {
   @DataProvider( name = "ValidMessages" )
   public Object[][] validMessages()
@@ -25,21 +21,22 @@ public class SyslogParserTest
         { "<34>1 2003-08-24T05:14:15.000003-07:00 mymachine.example.com su - ID47 - BOM'su root' failed for lonvick on /dev/pts/8" },
         { "<34>1 - mymachine.example.com su - ID47 - BOM'su root' failed for lonvick on /dev/pts/8" },
         { "<165>1 2003-08-24T05:14:15.000003-07:00 192.0.2.1 myproc 8710 - - %% It's time to make the do-nuts." },
-        { "<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"][examplePriority@32473 class=\"high\"]" }
+        { "<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"][examplePriority@32473 class=\"high\"]" },
+        { "<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"][examplePriority@32473 class=\"high\"] - and thats a wrap!" }
       };
   }
 
   @Test( dataProvider = "ValidMessages" )
   public void parseValidMessages( final String rawMessage )
   {
-    SyslogParser.parseSyslogMessage( rawMessage );
+    SyslogMessage.parseSyslogMessage( rawMessage );
   }
 
   @Test
   public void simpleParse()
   {
     final SyslogMessage message =
-      SyslogParser.parseSyslogMessage( "<34>1 2003-10-11T22:14:15.003Z mymachine.example.com su - ID47 [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"][examplePriority@32473 class=\"high\"] BOM'su root' failed for lonvick on /dev/pts/8" );
+      SyslogMessage.parseSyslogMessage( "<34>1 2003-10-11T22:14:15.003Z mymachine.example.com su - ID47 [exampleSDID@32473 iut=\"3\" eventSource=\"Application\" eventID=\"1011\"][examplePriority@32473 class=\"high\"] BOM'su root' failed for lonvick on /dev/pts/8" );
     assertEquals( message.getFacility(), 4 );
     assertEquals( message.getLevel(), 2 );
     final Date timestamp = message.getTimestamp();
@@ -60,6 +57,11 @@ public class SyslogParserTest
     assertEquals( p1.get( 1 ).getValue(), "Application" );
     assertEquals( p1.get( 2 ).getName(), "eventID" );
     assertEquals( p1.get( 2 ).getValue(), "1011" );
+    final List<StructuredDataParameter> p2 = structuredData.get( "examplePriority@32473" );
+    assertNotNull( p2 );
+    assertEquals( p2.size(), 1 );
+    assertEquals( p2.get( 0 ).getName(), "class" );
+    assertEquals( p2.get( 0 ).getValue(), "high" );
     assertEquals( message.getMessage(), "BOM'su root' failed for lonvick on /dev/pts/8" );
   }
 }
