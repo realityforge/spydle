452597bf5ebbe3f0f7a0f6ea680f87f5007c03b9
Peter Donald <peter@realityforge.org>
2012-07-20 11:57:07 +1000

    Remove unused import

--- src/main/java/org/realityforge/spydle/syslog/SyslogMessage.java
+++ src/main/java/org/realityforge/spydle/syslog/SyslogMessage.java
@@ -1,11 +1,9 @@
 package org.realityforge.spydle.syslog;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
